f33c3b14aacd64e1ff9ad96f862a5fd0fb4e1ee9
Maryna <marina.vishnevska@gmail.com>
2017-08-10 00:36:58 +0300

    Base_functionality

---
+++ UkrPoshtaTestTask_solution/UkrPoshtaTestTask/Models/MessagesStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UkrPoshtaTestTask.Models
+{
+    public class MessagesStore
+    {
+        private const int storeUserSize = 10;
+        private const int storeGlobalSize = 20;
+
+        public List<Message> Messages { get; set; }
+
+        public MessagesStore()
+        {
+            Messages = new List<Message>();
+        }
+        public void Add(Message message)
+        {
+            if (Messages.Count == storeGlobalSize)
+            {
+                List<Message> tmp = new List<Message>();
+                tmp = Messages.GetRange(1, storeGlobalSize-1);
+                Messages = tmp;
+            }
+            Messages.Add(message);
+        }
+        
+
+    }
+}
